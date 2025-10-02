cb2a28855812f519a64110f608e33ee2f4f6e638
Wilgas ADJOVI <wilfrieda216@gmail.com>
2025-10-02 22:08:29 +0100

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
-// firebase.js
+// src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: ".firebaseapp.com",
-  projectId: "",
-  storageBucket: ".appspot.com",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "TA_CLE_API",
+  authDomain: "TON_PROJET.firebaseapp.com",
+  projectId: "TON_PROJET",
+  storageBucket: "TON_PROJET.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
