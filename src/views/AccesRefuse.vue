1edb33dc295352846155d0f45d1ff1f990ad9581
Wilgas ADJOVI <wilfrieda216@gmail.com>
2025-10-09 20:07:55 +0100

    AccesRefuse.vue

---
+++ src/views/AccesRefuse.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen flex flex-col items-center justify-center bg-gray-100 text-center">
+    <h1 class="text-4xl font-bold text-red-600 mb-4">🚫 Accès refusé</h1>
+    <p class="text-gray-600">Vous n’avez pas les autorisations nécessaires pour accéder à cette page.</p>
+    <router-link to="/" class="mt-4 text-blue-500 underline">Retour à la connexion</router-link>
+  </div>
+</template>
