d7d79c7761bb06487faffae425f2df7990ec6757
Wilgas ADJOVI <wilfrieda216@gmail.com>
2025-10-21 19:16:13 -0300

    de

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 via-indigo-700 to-purple-800 p-4">
-    <div class="bg-white/10 backdrop-blur-lg rounded-2xl shadow-2xl w-full max-w-sm p-8 text-white">
-      <h1 class="text-3xl font-bold text-center mb-6">🔥 Connexion</h1>
-      <p class="text-center text-sm text-gray-200 mb-6">
-        Accédez à votre espace sécurisé
-      </p>
+  <div >
+    <div >
+      <h1 >
+        
+      </h1>
 
-      <div class="flex flex-col gap-[5rem]">
+      <div >
         <input
           v-model="email"
           type="email"
           placeholder="Adresse e-mail"
-          class="w-full px-4 py-3 rounded-lg bg-white/20 placeholder-gray-300 text-white focus:ring-2 focus:ring-blue-400 outline-none transition"
+          
         />
+
         <input
           v-model="password"
           type="password"
           placeholder="Mot de passe"
-          class="w-full px-4 py-3 rounded-lg bg-white/20 placeholder-gray-300 text-white focus:ring-2 focus:ring-blue-400 outline-none transition mt-[8rem]"
+          
         />
-        <div>
-          <button
-            @click="login"
-            class="mt-[7rem] w-full py-3 mt-2 rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 font-semibold text-white shadow-md transition active:scale-95"
-          >
-            Connexion
-          </button>
-        </div>
-        <p v-if="errorMessage" class="text-red-400 text-center text-sm mt-2">
+
+        <button
+          @click="login"
+          
+        >
+          Se connecter
+        </button>
+
+        <p v-if="errorMessage" class="text-red-500 text-center text-sm mt-2">
           {{ errorMessage }}
         </p>
       </div>
     </div>
   </div>
 </template>
+
 
 <script setup>
 import { ref } from "vue";
